c388d8e2a1af96ac7f52d200deab7c4778412270
kirtidalave <dalavekirti@gmail.com>
2022-04-05 15:05:11 +0530

    register successfully

--- main.py
+++ main.py
@@ -4,15 +4,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def StudentDate():
-    return render_template("student.html")
+def hom():
+    return render_template("slash.html")
 
 @app.route("/search")
-def searchstudent():
+def sea():
     return render_template("search.html")
+
 @app.route("/deleted")
-def deletedstudent():
+def delete():
     return render_template("delete.html")
+
+@app.route("/register")
+def regis():
+    return render_template("register.html")
 
 if __name__=="__main__":
     app.run()
