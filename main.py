731f211f8a4790004dcce935fa645503f98d1b48
kirtidalave <dalavekirti@gmail.com>
2022-04-05 16:30:59 +0530

    register successfully

--- main.py
+++ main.py
@@ -1,42 +1,58 @@
-import flask
+
 from flask import Flask, render_template,request
+import sqlite3
+conn=sqlite3.connect("StudentManagementSystem.db")
+
+listOfTables=conn.execute("SELECT name from sqlite_master WHERE type='table' AND name='STUDENT' ").fetchall()
+
+if listOfTables!=[]:
+    print("Table Already Exists ! ")
+else:
+    conn.execute(''' CREATE TABLE STUDENT(
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    NAME TEXT,COLLEGE_NAME TEXT, BRANCH TEXT,
+    ADMNO INTEGER, PASSWORD TEXT, DOB TEXT
+   ); ''')
+print("Table has created")
 
 app = Flask(__name__)
 
 @app.route("/")
-def hom():
-    return render_template("slash.html")
+def StudentData():
+    return render_template("login.html")
 
-@app.route("/search")
-def sea():
-    return render_template("search.html")
-
-@app.route("/deleted")
-def delete():
-    return render_template("delete.html")
 
 @app.route("/register", methods = ["GET", "POST"])
 def regis():
     if request.method=="POST":
         getName=request.form["name"]
-        getAdmno = request.form["admno"]
-        getRollno = request.form["Rollno"]
+        getClgName = request.form["college_name"]
         getBranch = request.form["br"]
-        getSemester= request.form["sem"]
+        getAdmno= request.form["admno"]
         getDOB = request.form["dob"]
-        getUsername = request.form["username"]
         getPass= request.form["pass"]
+        getcnfPass= request.form["cnf_pass"]
         print(getName)
-        print(getAdmno)
+        print(getClgName)
         print(getBranch)
-        print(getRollno)
-        print(getSemester)
-        print(getSemester)
+        print(getAdmno)
         print(getDOB)
-        print(getUsername)
         print(getPass)
+        print(getcnfPass)
+        try:
+            conn.execute("INSERT INTO STUDENT(name,COLLEGE_NAME TEXT,BRANCH,ADMNO,PASSWORD,DOB) VALUES('"+getName+"','"+getClgName+"','"+getBranch+"','"+getAdmno+"','"+getPass+"','"+getDOB+"',)")
+        except Exception as e:
+            print(e)
+
     return render_template("register.html")
 
+@app.route("/search")
+def searchStudent():
+    return render_template("search.html")
+
+@app.route("/delete")
+def deleteStudent():
+    return render_template("delete.html")
 
 
 if __name__=="__main__":
